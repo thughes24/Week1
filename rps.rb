a3d3d4592dc181995e34397d883fdf0d3dba0693
Tom Hughes <thomas.peter.hughes@gmail.com>
2015-03-24 16:30:05 +1030

    Fixed Rock Paper Scissors

--- rps.rb
+++ rps.rb
@@ -8,7 +8,7 @@ def win_or_lose(choice1,choice2)
   else
     case choice1
       when "p" then puts choice2 == "s" ? "You went #{Choices[choice1]}, Computer went Scissors, you win!" : "You went #{Choices[choice1]}, computer went Rock, you lose!"
-      when "r" then puts choice2 == "s" ? "You went #{Choices[choice1]}, Computer went Scissors, you lose!" : "You went #{Choices[choice1]}, computer went Rock, you win!"
+      when "r" then puts choice2 == "s" ? "You went #{Choices[choice1]}, Computer went Scissors, you lose!" : "You went #{Choices[choice1]}, computer went Paper, you win!"
       when "s" then puts choice2 == "p" ? "You went #{Choices[choice1]}, Computer went Paper, you win!" : "You went #{Choices[choice1]}, computer went Rock, you lose!"
     end  
   end
@@ -21,12 +21,15 @@ puts "Lets play rock paper scissors!"
 begin
   puts "Make a choice (r,p,s)"
   player_choice = gets.chomp.downcase
-end until player_choice = Choices.keys.include?(player_choice)
+end until Choices.keys.include?(player_choice)
 
 computer_choice = Choices.keys.sample
 
 win_or_lose(player_choice,computer_choice)
 
 puts "play again? (y/n)"
-break if gets.chomp.downcase == 'n'
+if gets.chomp.downcase == 'n'
+  puts "Thanks for playing"
+  break
+end 
 end
